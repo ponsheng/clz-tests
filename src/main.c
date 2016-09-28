9f22faa82446a5e97a5a3723f9786f9de90f6b33
ponsheng <a7061120@gmail.com>
2016-09-28 15:28:04 +0800

    main.c delete output file

--- src/main.c
+++ src/main.c
@@ -13,21 +13,18 @@ int main()
     uint32_t num = 0;
 	int n =clz(num);
 	int is_check = 0;
-	printf("zeros: %d  \n",n);
-	printf("check:%d \n",check(num,n));
-	FILE * output= fopen ( "output.txt", "w");
-	printf("123");
+	//FILE * output= fopen ( "output.txt", "w");
 	int count = 0;
 	for(num=0 ; num < pow(2,32); num++)
 	{
 	    n = clz(num);
 		is_check = check(num,n);
-		if(!is_check)
-			printf("incorrect %u  %u\n",num,n);
 		if((num & (num - 1)) == 0)
 			printf("power of 2: %u\n",count++);
-		fprintf(output, "num %u zeros %u check %u\n",num,n,is_check );
+		if(!is_check)
+			printf("incorrect %u  %u\n",num,n);
+		//fprintf(output,  "%u %u %u\n",num,n,is_check );
 	}
-	fclose(output); 
+	//fclose(output); 
 	return 0;
 }
